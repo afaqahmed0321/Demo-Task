4ec54873e773417771bd3604b03e74d535ebb9fc
DESKTOP-K54MHV2\This Laptop <afaqahmed0321@gmail.com>
2024-12-19 18:42:29 +0500

    category section

--- src/components/categorySection.jsx
+++ src/components/categorySection.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
+import Dining from '../assets/DiningImage.png';
+import Salon from '../assets/SalonImage.png';
+import Entertainment from '../assets/EntertainmentImage.png';
+import Cleaning from '../assets/CleaningImage.png';
+
 
 const CategorySection = () => {
   const categories = [
-    { icon: "path-to-dinning-icon", label: "Dining" },
-    { icon: "path-to-saloon-icon", label: "Salon/Spa" },
-    { icon: "path-to-entertainment-icon", label: "Entertainment" },
-    { icon: "path-to-cleaning-icon", label: "Cleaning" },
-    { icon: "path-to-cleaning-icon", label: "Cleaning" },
-    { icon: "path-to-cleaning-icon", label: "Cleaning" },
-    { icon: "path-to-cleaning-icon", label: "Cleaning" },
-    { icon: "path-to-cleaning-icon", label: "Cleaning" },
+    { icon: Dining, label: "Dining" },
+    { icon: Salon, label: "Salon/Spa" },
+    { icon: Entertainment, label: "Entertainment" },
+    { icon: Cleaning, label: "Cleaning" },
+    { icon: Cleaning, label: "Cleaning" },
+    { icon: Cleaning, label: "Cleaning" },
+    { icon: Cleaning, label: "Cleaning" },
   ];
 
   return (
     <div className=" py-12">
-      <div className="container mx-auto">
+      <div className="container-fluid mx-auto">
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-6">
           {categories.map((category, index) => (
             <div
               key={index}
               className="flex flex-col items-center text-center p-4 bg-white  rounded-xl transition duration-300"
             >
-              <div className="bg-blue-100 p-4 rounded-full mb-4">
-                <img src={category.icon} alt={category.label} className="h-10 w-10" />
+              <div className="">
+                <img src={category.icon} alt={category.label} className=" p-4 rounded-full mb-4" />
               </div>
               <span className="text-sm font-medium text-gray-800">{category.label}</span>
             </div>
